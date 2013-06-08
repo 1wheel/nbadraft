f32b7049c148dac8cf60b177e10d1e46b4749334
Adam Pearce <adam.r.pearce@gmail.com>
2013-06-08 00:23:31 -0500

    image scrapping set up

--- imagesDL.py
+++ imagesDL.py
@@ -1,4 +1,6 @@
 import pyBingSearchAPI
+import json
+from time import sleep
 
 key = 'uzMuSoVwFFEbXSo/lRT89rvLlFy6KG2C8q2s+PyN4h4'
 bing = pyBingSearchAPI.BingSearchAPI(key)
@@ -11,19 +13,31 @@ params = {'ImageFilters':'"Face:Face+Aspect:Square"',
 
 r = bing.search('image', 'Tim Duncan nba', params)
 
+with open('imgSearchResults.json') as data_file2:    
+	imgSearchResults = json.load(data_file2)
 
 with open('webpage/drafts.json') as data_file:    
 	drafts = json.load(data_file)
 
-
-draftPics = {}
-
 for draft in drafts:
 	for player in draft['players']:
-		if url in player:
-			key = player['url']
+		if 'url' in player:
+			key = player['url'][11:-5]
 		else:
 			key = player['name']
 
-		draftPics[key] = r = bing.search('image', player['name'] + 'nba', params)
-		print key
+		try:
+			if key not in imgSearchResults:
+				print key
+				imgSearchResults[key] = bing.search('image', player['name'] + 'nba', params)
+				sleep(.1)
+
+		except Exception, e:
+			print e
+			with open('imgSearchResults.json', 'w') as outfile:
+				json.dump(imgSearchResults, outfile)
+			print "!!!!" + key + "!!!!"
+			sleep(10)
+
+with open('imgSearchResults.json', 'w') as outfile:
+	json.dump(imgSearchResults, outfile)
